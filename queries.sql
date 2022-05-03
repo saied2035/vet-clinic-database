bfe13f44ffd45aea0cdba5dd689c089c998c5df6
unknown <saied2421998@gmail.com>
2022-05-03 15:24:42 +0200

    fINISH required queries

--- queries.sql
+++ queries.sql
@@ -8,3 +8,10 @@ SELECT name,escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE NOT name  = 'Gabumon';
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+SELECT COUNT(*) FROM animals;
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0; 
+SELECT AVG(weight_kg) AS avrage FROM animals;
+SELECT neutered, COUNT(escape_attempts) AS count FROM animals WHERE escape_attempts > 0 GROUP BY neutered;
+SELECT species,MIN(weight_kg),MAX(weight_kg) FROM animals GROUP BY species;
+SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth >= '1990-1-1' 
+AND date_of_birth <= '2000-12-31' GROUP BY species;
