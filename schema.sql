957d5eed44566ce0aeab0ad49fa524e074794d31
unknown <saied2421998@gmail.com>
2022-05-04 10:49:40 +0200

    Create owners & species tables. Modify animals table to have foeign keys for owner and species tables

--- schema.sql
+++ schema.sql
@@ -12,3 +12,28 @@ CREATE TABLE animals (
 
 ALTER TABLE animals
 ADD COLUMN species VARCHAR(20);
+
+CREATE TABLE owners (
+ id INT GENERATED ALWAYS AS IDENTITY,
+ full_name varchar(100) NOT NULL,
+ age SMALLINT NOT NULL,
+ PRIMARY KEY (id)
+);
+
+CREATE TABLE species (
+ id INT GENERATED ALWAYS AS IDENTITY,
+ name varchar(100) NOT NULL,
+ PRIMARY KEY (id)
+);
+
+ALTER TABLE animals
+DROP COLUMN species;
+
+ALTER TABLE animals
+ADD FOREIGN KEY (species_id) REFERENCES species (id)
+ON DELETE CASCADE;
+
+ALTER TABLE animals
+ADD CONSTRAINT fk_owners
+FOREIGN KEY (owner_id) REFERENCES owners(id)
+ON DELETE CASCADE;
